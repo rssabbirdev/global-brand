624fef518bc1792074a90ead6cbe20d840a932ed
Developer-Shourav <developer.shourav1@gmail.com>
2023-11-21 16:44:45 +0600

    testing component added

---
+++ src/components/NewCompo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewCompo = () => {
+    return (
+        <div>
+            <h2>New Component added for testing purpose</h2>
+        </div>
+    );
+};
+
+export default NewCompo;
